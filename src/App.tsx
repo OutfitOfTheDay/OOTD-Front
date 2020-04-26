5bb282f98a22ad60e33e3de267f39ce8563633dd
SuHaLee <gimtommang88@gmail.com>
2020-04-26 19:08:53 +0900

    Create App,tsx, index.tsx, index.html
    기본 파일 생성

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div>hello react</div>;
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import "@babel/polyfill";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.tsx";
+
+ReactDOM.render(<App />, document.getElementById("root"));
