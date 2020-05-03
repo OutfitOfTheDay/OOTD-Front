6d9ac2bde2a4e73ca4a2d24a5026789a1abf48c8
SuHaLee <gimtommang88@gmail.com>
2020-05-03 18:43:49 +0900

    change double quotes to single quites

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,5 @@
-import * as React from "react";
-import { Redirect, Route, Switch } from "react-router-dom";
+import * as React from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 const Router: React.FC = () => (
   <Switch>
