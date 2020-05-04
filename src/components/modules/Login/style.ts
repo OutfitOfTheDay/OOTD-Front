9b5af07df517f6ddacb8956e686353cc75314d62
SuHaLee <gimtommang88@gmail.com>
2020-05-04 20:11:11 +0900

    add size loginModal

--- src/components/modules/Login/style.ts
+++ src/components/modules/Login/style.ts
@@ -2,17 +2,18 @@ import styled from 'styled-components';
 
 export const LoginModalWrapper = styled.div`
   display: flex;
-  background-color: #fff;
-  border: 1px solid black;
-  justify-content: center;
-  align-items: center;
   width: 25rem;
   height: 17.5rem;
+  background-color: #fff;
+  border: 1px solid black;
+  flex-direction: column;
+  align-items: center;
 `;
 
 export const LoginButtonsWrapper = styled.div`
   display: flex;
   height: 10rem;
+  margin-top: 1.875rem;
   flex-direction: column;
   justify-content: space-between;
 `;
