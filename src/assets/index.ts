f7cdf082c7de5a7acb8e2f9605987b2d1aef4314
sohi_5 <38098147+soheejang15@users.noreply.github.com>
2020-05-05 23:12:05 +0900

    Add assets

---
+++ src/assets/index.ts
@@ -0,0 +1,10 @@
+import { disabled, enabled } from './weather_icon';
+import reset from './weather_icon/reset/reset.png';
+import temperature from './weather_icon/temperature/temperature.png';
+
+export const weather = {
+  disabled,
+  enabled,
+  reset,
+  temperature,
+};
