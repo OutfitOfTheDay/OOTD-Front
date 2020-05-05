4090b08f029a3c811853d41d078e653c80f43fd5
sohi_5 <38098147+soheejang15@users.noreply.github.com>
2020-05-05 23:12:05 +0900

    Add assets

---
+++ src/assets/weather_icon/index.ts
@@ -0,0 +1,26 @@
+import clear_disabled from './clear/clear_disabled.png';
+import clear_enabled from './clear/clear_enabled.png';
+import cloudy_disabled from './cloudy/cloudy_disabled.png';
+import cloudy_enabled from './cloudy/cloudy_enabled.png';
+import rain_disabled from './rain/rain_disabled.png';
+import rain_enabled from './rain/rain_enabled.png';
+import rain_snow_disabled from './rainSnow/rainSnow_disabled.png';
+import rain_snow_enabled from './rainSnow/rainSnow_enabled.png';
+import snow_disabled from './snow/snow_disabled.png';
+import snow_enabled from './snow/snow_enabled.png';
+
+export const disabled = {
+  clear: clear_disabled,
+  cloudy: cloudy_disabled,
+  rain: rain_disabled,
+  rain_snow: rain_snow_disabled,
+  snow: snow_disabled,
+};
+
+export const enabled = {
+  clear: clear_enabled,
+  cloudy: cloudy_enabled,
+  rain: rain_enabled,
+  rain_snow: rain_snow_enabled,
+  snow: snow_enabled,
+};
