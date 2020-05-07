936cd03835fb1e311aac32e0e22dae0637eb0e91
SuHaLee <gimtommang88@gmail.com>
2020-05-08 02:46:42 +0900

    resizing modal button wrapper

--- src/components/modules/ModalButtonWrapper/style.ts
+++ src/components/modules/ModalButtonWrapper/style.ts
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 export const ModalButtonWrapper = styled.div`
   display: flex;
+  width: inherit;
   height: 3.75rem;
   border-top: 0.0625rem solid #e9e0f2;
+  justify-content: flex-end;
 `;
