d9a02e84d9284dcf76244d40f74e3820fbfbc5ee
SuHaLee <gimtommang88@gmail.com>
2020-05-08 02:53:57 +0900

    modify Header content Spacing method

--- src/components/modules/ModalHeader/style.ts
+++ src/components/modules/ModalHeader/style.ts
@@ -6,12 +6,12 @@ export const ModalHeaderWrapper = styled.div`
   width: inherit;
   height: 3.125rem;
   align-items: center;
+  justify-content: space-between;
 `;
 
 export const ModalName = styled.p`
   color: ${MainColor};
   font-size: 1.125rem;
-  margin-left: 10.94rem;
   font-weight: 800;
 `;
 
@@ -19,5 +19,4 @@ export const closeButton = styled.img`
   width: 1.25em;
   height: 1.25em;
   background-color: #a8a8a8;
-  margin-left: 8.813rem; 
 `;
