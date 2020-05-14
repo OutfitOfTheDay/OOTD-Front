a3d2e56cb63300f1319ffec40a2228a086c681ca
SuHaLee <gimtommang88@gmail.com>
2020-05-15 00:54:59 +0900

    add modal atoms index.ts

---
+++ src/components/atoms/modals/index.ts
@@ -0,0 +1,6 @@
+import ModalButton from './ModalButton/index';
+import ModalContentWrapper from './ModalContentWrapper/index';
+import ModalOverlay from './ModalOverlay/index';
+import ModalHeader from './ModalHeader/index';
+
+export { ModalButton, ModalContentWrapper, ModalOverlay, ModalHeader };
