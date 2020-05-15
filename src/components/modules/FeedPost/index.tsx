974d43ac45ab4524989032883be5a597c925aebe
SuHaLee <gimtommang88@gmail.com>
2020-05-15 22:12:46 +0900

    create feedPost Contaienr

---
+++ src/components/modules/FeedPost/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import * as S from './style';
+import { ImgNumberWrapper, FeedImg } from '../../atoms/Feeds/index';
+
+interface Props {}
+
+const FeedPost: React.FC<Props> = ({}) => {
+  return (
+    <S.FeedWrapper>
+      <FeedImg ImgUrl="https://i.pinimg.com/564x/50/a2/ea/50a2eae753e75ef5679f6530129a34d2.jpg" />
+      <ImgNumberWrapper />
+      <S.Dummy />
+    </S.FeedWrapper>
+  );
+};
+
+export default FeedPost;
---
+++ src/components/modules/FeedPost/style.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const FeedWrapper = styled.div`
+  width: 43.75rem;
+  hight: auto;
+  border: 1px solid #55109b;
+  box-shadow: 0px 5px 10px #d3d3d3;
+`;
+
+export const Dummy = styled.div`
+  background-color: #fff;
+  height: 12.5rem;
+`;
