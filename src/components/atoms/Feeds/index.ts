8e15f39fd7c014e8e2cf7b5a3cc402650ba62ad1
SuHaLee <gimtommang88@gmail.com>
2020-05-15 22:13:41 +0900

    create feed atom index.ts

---
+++ src/components/atoms/Feeds/index.ts
@@ -0,0 +1,5 @@
+import ImgNumber from './ImgNumber';
+import ImgNumberWrapper from './ImgNumberWrapper/index';
+import FeedImg from './FeedImg/index';
+
+export { ImgNumber, ImgNumberWrapper, FeedImg };
