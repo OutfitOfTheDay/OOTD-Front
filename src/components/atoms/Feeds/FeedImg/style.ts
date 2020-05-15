992850c978174af3d6b3ae71ee75bb0e15b80c27
SuHaLee <gimtommang88@gmail.com>
2020-05-15 22:13:17 +0900

    Modify FeedImg css
    이미지에 맞게 고정된 widht와 변동되는 height를 가지도록 변경

--- src/components/atoms/Feeds/FeedImg/style.ts
+++ src/components/atoms/Feeds/FeedImg/style.ts
@@ -1,17 +1,10 @@
 import styled from 'styled-components';
 import { DeactColor } from '../../../../GlobalStyle';
 export const FeedImageWrapper = styled.div`
-  display: flex;
-  background-color: #505144;
   width: 43.75rem;
-  height: 43.75rem;
-  overflow: hidden;
-  justify-content: center;
-  align-items: center;
 `;
 
 export const FeedImage = styled.img`
-  object-fit: contain;
-  max-width: inherit;
-  height: inherit;
+  width: 100%;
+  height: auto;
 `;
