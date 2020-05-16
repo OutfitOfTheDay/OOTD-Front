7e3cbf39c38e3f887c093e4098419cf3d786af3b
SuHaLee <gimtommang88@gmail.com>
2020-05-17 00:29:18 +0900

    Add component FeedPost

--- src/components/modules/FeedPost/index.tsx
+++ src/components/modules/FeedPost/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import * as S from './style';
-import { ImgNumberWrapper, FeedImg } from '../../atoms/Feeds/index';
+import {
+  ImgNumberWrapper,
+  FeedImg,
+  WriterInfo,
+  TagCommentCount,
+  PostContent,
+} from '../../atoms/Feeds/index';
 
 interface Props {}
 
@@ -8,8 +14,12 @@ const FeedPost: React.FC<Props> = ({}) => {
   return (
     <S.FeedWrapper>
       <FeedImg ImgUrl="https://i.pinimg.com/564x/50/a2/ea/50a2eae753e75ef5679f6530129a34d2.jpg" />
+      <WriterInfo />
       <ImgNumberWrapper />
-      <S.Dummy />
+      <S.Dummy>
+        <TagCommentCount />
+        <PostContent />
+      </S.Dummy>
     </S.FeedWrapper>
   );
 };
