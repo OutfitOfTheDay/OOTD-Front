6b70513c571924f6744eaa3e15594b8377604f5f
SuHaLee <gimtommang88@gmail.com>
2020-05-17 00:20:25 +0900

    Create Postcontent Component

---
+++ src/components/atoms/Feeds/PostContent/style.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+import { MainColor, DeactColor } from '../../../../GlobalStyle';
+
+export const PostContentWrapper = styled.div`
+  display: flex;
+  width: inherit;
+  flex-direction: row;
+  align-items: flex-end;
+  justify-content: space-between;
+`;
+
+export const PostContent = styled.p`
+  width: 34rem;
+  height: 5.125rem;
+  overflow: hidden;
+  line-height: 17.5px;
+  white-space: pre-line;
+  font-size: 0.875rem;
+  color: ${DeactColor};
+`;
+
+export const ShowDetailPostBtn = styled.button`
+  font-size: 0.875rem;
+  color: ${MainColor};
+  font-weight: bold;
+`;
