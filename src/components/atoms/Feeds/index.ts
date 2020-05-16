c73df6b3d9c6587ff2219fbb05452f5e40eab772
SuHaLee <gimtommang88@gmail.com>
2020-05-17 00:29:02 +0900

    add component index.ts

--- src/components/atoms/Feeds/index.ts
+++ src/components/atoms/Feeds/index.ts
@@ -1,5 +1,15 @@
 import ImgNumber from './ImgNumber';
 import ImgNumberWrapper from './ImgNumberWrapper/index';
 import FeedImg from './FeedImg/index';
+import WriterInfo from './WriterInfo/index';
+import TagCommentCount from './TagCommentCount/index';
+import PostContent from './PostContent/index';
 
-export { ImgNumber, ImgNumberWrapper, FeedImg };
+export {
+  ImgNumber,
+  ImgNumberWrapper,
+  FeedImg,
+  WriterInfo,
+  TagCommentCount,
+  PostContent,
+};
