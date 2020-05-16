beab5828d5e9149867f376753eac47a30e3981d5
SuHaLee <gimtommang88@gmail.com>
2020-05-16 22:25:27 +0900

    add property

--- src/components/modules/FeedPost/style.ts
+++ src/components/modules/FeedPost/style.ts
@@ -1,10 +1,13 @@
 import styled from 'styled-components';
 
 export const FeedWrapper = styled.div`
+  display: flex;
   width: 43.75rem;
-  hight: auto;
+  height: auto;
   border: 1px solid #55109b;
   box-shadow: 0px 5px 10px #d3d3d3;
+  align-items: center;
+  flex-direction: column;
 `;
 
 export const Dummy = styled.div`
