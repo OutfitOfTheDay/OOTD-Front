8539da4c71fcf883e4127622d88ab5d9cbda653b
SuHaLee <gimtommang88@gmail.com>
2020-05-17 20:03:45 +0900

    add movebutton in Img

--- src/components/atoms/Feeds/FeedImg/index.tsx
+++ src/components/atoms/Feeds/FeedImg/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import * as S from './style';
 import WriterInfo from '../WriterInfo';
+import FeedImgMoveBtn from '../FeedImgMoveBtn';
+import leftMoveButton from '../../../../assets/Feed_icon/left_move_button.png';
+import rightMoveButton from '../../../../assets/Feed_icon/right_move_button.png';
 
 interface Props {
   ImgUrl: string;
@@ -13,6 +16,8 @@ const FeedImg: React.FC<Props> = ({ ImgUrl }) => {
   return (
     <S.FeedImageWrapper>
       <S.FeedImage src={ImgUrl} />
+      <FeedImgMoveBtn ButtonImg={leftMoveButton} />
+      <FeedImgMoveBtn isRight ButtonImg={rightMoveButton} />
       <WriterInfo />
     </S.FeedImageWrapper>
   );
