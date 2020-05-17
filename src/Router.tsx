ed3c550e82e3d4ca9b45771f2925319ac06c511f
SuHaLee <gimtommang88@gmail.com>
2020-05-17 23:52:22 +0900

    add Feed component in main router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
+import Feed from './components/templates/Feed';
 
 const Router: React.FC = () => (
   <Switch>
-    <Route exact path="/" component={() => <div>메인입니당</div>} />
+    <Route exact path="/" component={() => <Feed />} />
     <Route component={() => <Redirect to="/" />} />
   </Switch>
 );
