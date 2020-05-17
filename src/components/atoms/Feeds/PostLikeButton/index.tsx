e0cf5cd15d2e9ddf6f0aeea9653735dc237d8929
SuHaLee <gimtommang88@gmail.com>
2020-05-17 20:04:26 +0900

    add tag/ comment btn

--- src/components/atoms/Feeds/PostLikeButton/index.tsx
+++ src/components/atoms/Feeds/PostLikeButton/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import * as S from './style';
+// import { feed } from '../../../../assets/index';
+import tag from '../../../../assets/Feed_icon/tag.png';
+import comment from '../../../../assets/Feed_icon/comment.png';
 
 const PostLikeButton: React.FC = () => {
   return (
     <S.PostButtonWrapper>
-      <S.PostButton />
-      <S.PostButton />
+      <S.PostButton src={tag} alt="tagButton" />
+      <S.PostButton src={comment} alt="commentButton" />
     </S.PostButtonWrapper>
   );
 };
