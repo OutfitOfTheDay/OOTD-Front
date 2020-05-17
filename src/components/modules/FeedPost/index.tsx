2780694cf5db9ccef96f70148400d89a62ba60e8
SuHaLee <gimtommang88@gmail.com>
2020-05-17 20:05:05 +0900

    add Likebutton in Post

--- src/components/modules/FeedPost/index.tsx
+++ src/components/modules/FeedPost/index.tsx
@@ -3,9 +3,9 @@ import * as S from './style';
 import {
   ImgNumberWrapper,
   FeedImg,
-  WriterInfo,
   TagCommentCount,
   PostContent,
+  PostLikeButton,
 } from '../../atoms/Feeds/index';
 
 interface Props {}
@@ -14,9 +14,9 @@ const FeedPost: React.FC<Props> = ({}) => {
   return (
     <S.FeedWrapper>
       <FeedImg ImgUrl="https://i.pinimg.com/564x/50/a2/ea/50a2eae753e75ef5679f6530129a34d2.jpg" />
-      <WriterInfo />
       <ImgNumberWrapper />
       <S.Dummy>
+        <PostLikeButton />
         <TagCommentCount />
         <PostContent />
       </S.Dummy>
