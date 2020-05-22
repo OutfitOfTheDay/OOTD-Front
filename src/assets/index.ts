f7b3098e661cf62e7723a5f4003e970a5712b8cf
SuHaLee <gimtommang88@gmail.com>
2020-05-22 14:15:47 +0900

    add feed img object in index.ts

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,10 +1,21 @@
 import { disabled, enabled } from './weather_icon';
 import reset from './weather_icon/reset/reset.png';
 import temperature from './weather_icon/temperature/temperature.png';
+import comment from './Feed_icon/comment.png';
+import tag from './Feed_icon/tag.png';
+import left_move_button from './Feed_icon/left_move_button.png';
+import right_move_button from './Feed_icon/right_move_button.png';
 
 export const weather = {
   disabled,
   enabled,
   reset,
   temperature,
+};
+
+export const feed = {
+  comment,
+  tag,
+  left_move_button,
+  right_move_button,
 };
