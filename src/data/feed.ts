135cff9928881a3548667d941f042884e54b8228
SuHaLee <gimtommang88@gmail.com>
2020-05-22 14:49:31 +0900

    modify dummy value

--- src/data/feed.ts
+++ src/data/feed.ts
@@ -27,12 +27,12 @@ const initialState: FeedState = {
   feedList: [
     {
       _id: 'qwer',
-      userId: '김이름',
+      userId: '김a름',
       profile:
         'https://cdn.allets.com/500/2018/08/27/500_322525_1535357464.jpeg',
       content: ' string',
-      likeN: 2002,
-      cmtN: 1002,
+      likeN: 202,
+      cmtN: 102,
       pictures: [
         'https://i.pinimg.com/564x/50/a2/ea/50a2eae753e75ef5679f6530129a34d2.jpg',
         'https://img9.yna.co.kr/etc/inner/KR/2019/04/08/AKR20190408066300073_01_i_P2.jpg',
@@ -46,17 +46,38 @@ const initialState: FeedState = {
       },
     },
     {
+      _id: 's',
+      userId: '김b름',
+      profile:
+        'https://cdn.allets.com/500/2018/08/27/500_322525_1535357464.jpeg',
+      content: ' string',
+      likeN: 2,
+      cmtN: 1002,
+      pictures: [
+        'https://i.pinimg.com/originals/2e/5b/01/2e5b01db65071915dff659cf5f4fa3f1.png',
+        'https://img.insight.co.kr/static/2017/10/11/700/4565ef482f2pb91l8u30.jpg',
+        'https://dimg.donga.com/i/600/0/90/ugc/CDB/WEEKLY/Article/5c/a6/e3/1c/5ca6e31c198dd2738de6.jpg',
+      ],
+      date: 'string',
+      weather: {
+        status: 1,
+        temp: 16,
+      },
+    },
+    {
       _id: 'qwer',
-      userId: '김이름',
+      userId: '김c름',
       profile:
         'https://cdn.allets.com/500/2018/08/27/500_322525_1535357464.jpeg',
       content: ' string',
       likeN: 2002,
       cmtN: 1002,
       pictures: [
-        'https://img.insight.co.kr/static/2017/10/11/700/4565ef482f2pb91l8u30.jpg',
-        'https://lh3.googleusercontent.com/proxy/e7KDm2vay__zykIV2wQo0dFnboPhcQv8u3fpix8QfW0yt82VXHtbWWDKaeLIoIxh0zVAYCq5RE9Gh_r6inv7mPB5IuMvmaVKq2dTHfQzOpscdMdXYEyz5qnFfYIDvR-4GyI8uDEYBMOU40LoleoKU4Pl519SMn6j_7GOvy74nJUmNVE-XFr14R9eaqZxnAQllZzEHqqIACzLBhU_QuYnL1G95HW_wFgP-4_bZPjwZf6-26hgx_PNo4JodWKXje2m2YV_i9RirxHjgsRExCvLwkQVmC_lGFfByrfcynIix-n4acmt-6oQSsQWYHJ0MF0oymq8pEUi',
+        'https://dimg.donga.com/i/600/0/90/ugc/CDB/WEEKLY/Article/5c/a6/e3/1c/5ca6e31c198dd2738de6.jpg',
         'https://i.pinimg.com/originals/2e/5b/01/2e5b01db65071915dff659cf5f4fa3f1.png',
+        'https://img.insight.co.kr/static/2017/10/11/700/4565ef482f2pb91l8u30.jpg',
+        'https://img.insight.co.kr/static/2017/10/11/700/4565ef482f2pb91l8u30.jpg',
+        'https://img.insight.co.kr/static/2017/10/11/700/4565ef482f2pb91l8u30.jpg',
       ],
       date: 'string',
       weather: {
