005ebda811afdaaf94ee55f87a0f1da68256a4c1
gimtommang11 <gimtommang88@gmail.com>
2020-06-10 20:23:44 +0900

    add header in feed

--- src/components/templates/Feed/index.tsx
+++ src/components/templates/Feed/index.tsx
@@ -2,18 +2,15 @@ import React, { useEffect } from 'react';
 import * as S from './style';
 import FeedPost from '../../modules/FeedPost';
 import useFeed from '../../../hooks/useFeed';
+import Header from 'modules/Header';
 import { getFeedData } from '../../../data/api/getFeedData';
 
 const Feed: React.FC = () => {
   const { feedList } = useFeed();
-  useEffect(() => {
-    getFeedData().then((response) => {
-      // onGetFeed(response[0]);
-      console.log(response);
-    });
-  }, []);
+
   return (
     <S.FeedContainer>
+      <Header />
       {feedList.map((feedPost, index) => (
         <FeedPost post={feedPost} key={index} postId={index} />
       ))}
