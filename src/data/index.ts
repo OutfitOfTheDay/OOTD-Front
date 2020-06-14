1a6bcf174ff19f832f3cb9c6b2b24d1aa04fcd9d
gimtommang11 <gimtommang88@gmail.com>
2020-06-14 18:57:17 +0900

    add feedSaga in index.ts

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,10 +2,12 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import reduxSaga from 'redux-saga';
 import { all, call } from 'redux-saga/effects';
-import feed, { FeedState } from './feed';
+
+import feed, { FeedState } from './feed/feed';
 import detailPost, { DetailPostState } from './detailPost';
 import comment, { CommentState } from './comment/comment';
 import { commentSaga } from './comment/commentSaga';
+import { feedSaga } from './feed/feedSaga';
 
 export interface StoreState {
   feed: FeedState;
