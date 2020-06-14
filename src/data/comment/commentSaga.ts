279087a257d3fd837b39e409a1c5378853caad8b
gimtommang11 <gimtommang88@gmail.com>
2020-06-14 18:56:07 +0900

    crete add comment saga

--- src/data/comment/commentSaga.ts
+++ src/data/comment/commentSaga.ts
@@ -1,11 +1,11 @@
 import { all, fork, takeLatest, call, put } from 'redux-saga/effects';
 
-import { getCommentData } from '../api/index';
+import { getCommentData, writingComment } from '../api/index';
 import * as comment from './comment';
 
-function* getComment(action: comment.CommentAction) {
+function* getComment(action: comment.GetComment) {
   try {
-    const commentData = yield call(getCommentData, action.payload);
+    const commentData = yield call(getCommentData, action.payload.postId);
     yield put({
       type: comment.GET_COMMENT_SUCCESS,
       payload: commentData,
@@ -18,6 +18,26 @@ function* getComment(action: comment.CommentAction) {
   }
 }
 
+function* addComment(action: comment.AddComment) {
+  try {
+    const addCommentStatus = yield call(
+      writingComment,
+      action.payload.text,
+      action.payload.postId,
+    );
+    yield put({
+      type: comment.ADD_COMMENT_SUCCESS,
+      payload: addCommentStatus,
+    });
+  } catch (error) {
+    yield put({
+      type: comment.ADD_COMMENT_FAILURE,
+      payload: error,
+    });
+  }
+}
+
 export function* commentSaga() {
   yield takeLatest(comment.GET_COMMENT, getComment);
+  yield takeLatest(comment.ADD_COMMENT, addComment);
 }
