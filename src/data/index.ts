239af4a7d8bcebd752914879fc48a08561a30186
gimtommang11 <gimtommang88@gmail.com>
2020-06-16 22:37:20 +0900

    modify data index.ts

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,12 +1,12 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { applyMiddleware, combineReducers, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import reduxSaga from 'redux-saga';
+import createSagaMiddleware from 'redux-saga';
 import { all, call } from 'redux-saga/effects';
 
 import feed, { FeedState } from './feed/feed';
 import detailPost, { DetailPostState } from './detailPost';
 import comment, { CommentState } from './comment/comment';
+import feedSortReducer from './modules/FeedSort';
 import { commentSaga } from './comment/commentSaga';
 import { feedSaga } from './feed/feedSaga';
 
@@ -20,20 +20,13 @@ const rootReducer = combineReducers({
   feed,
   detailPost,
   comment,
-});
-import createSagaMiddleware from 'redux-saga';
-import { all, call } from 'redux-saga/effects';
-
-const sagaMiddleWare = reduxSaga();
-import feedSortReducer from './modules/FeedSort';
-const rootReducer = combineReducers({
   feedSortReducer,
 });
 const sagaMiddleWare = createSagaMiddleware();
 function* rootSaga() {
   yield all([
-    call(weatherStatus),
-    call(postUpload),
+    // call(weatherStatus),
+    // call(postUpload),
     call(feedSaga),
     call(commentSaga),
   ]);
