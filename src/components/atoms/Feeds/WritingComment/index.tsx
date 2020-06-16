e8b0ffa9304f39c3f80b1ed6dea43cfa81318632
gimtommang11 <gimtommang88@gmail.com>
2020-06-16 15:57:33 +0900

    add submmit comment with press enter

--- src/components/atoms/Feeds/WritingComment/index.tsx
+++ src/components/atoms/Feeds/WritingComment/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent } from 'react';
+import React, { useState, ChangeEvent, KeyboardEvent } from 'react';
 
 import * as S from './style';
 import useComment from '../../../../hooks/useComment';
@@ -10,22 +10,39 @@ interface Props {
 
 const WritingComment: React.FC<Props> = ({ postId }) => {
   const [comment, setComment] = useState('');
-  const { postIndex } = usePost();
   const { onAddComment } = useComment();
 
   const addComment = () => {
     onAddComment(comment, postId);
+    setComment('');
+  };
+
+  const addCommentKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      addComment();
+      console.log(comment);
+    }
   };
   const onChangeComment = (e: ChangeEvent<HTMLInputElement>) => {
     setComment(e.target.value);
   };
+
+  // const isWritingCommentExist = () => {
+  //   if (comment.length > 0) {
+  //     console.log('asdf');
+  //     return true;
+  //   } else return false;
+  // };
+
   return (
     <S.CommentWritingWrapper>
       <S.WritingCommentInput
         placeholder="댓글달기"
+        value={comment}
         onChange={onChangeComment}
+        onKeyPress={(e) => addCommentKeyPress(e)}
       ></S.WritingCommentInput>
-      <S.PostingCommentButton onClick={() => addComment()}>
+      <S.PostingCommentButton onClick={() => addComment()} comment={comment}>
         게시
       </S.PostingCommentButton>
     </S.CommentWritingWrapper>
