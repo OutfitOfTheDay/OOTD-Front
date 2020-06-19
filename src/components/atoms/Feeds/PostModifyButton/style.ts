2e0b47ec181d36b259b293f8ce97788986564343
gimtommang11 <gimtommang88@gmail.com>
2020-06-19 11:09:35 +0900

    Add hover style  PostModifyButton

--- src/components/atoms/Feeds/PostModifyButton/style.ts
+++ src/components/atoms/Feeds/PostModifyButton/style.ts
@@ -8,6 +8,10 @@ export const PostModifyButtonWrapper = styled.div`
 
 export const PostModifyButton = styled.button<{ fontSize: number }>`
   font-size: ${(props) => props.fontSize}rem;
-  color: ${MainColor};
+  color: rgba(85, 16, 155, 0.6);
+  font-weight: 800;
   margin-left: 0.5rem;
+  &: hover {
+    color: ${MainColor};
+  }
 `;
