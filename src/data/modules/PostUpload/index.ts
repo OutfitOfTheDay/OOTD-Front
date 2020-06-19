8693e1fe2f84b4d450fa5c9e7ec0c142d4aa66f9
so_ohi <jsh167528@gmail.com>
2020-06-19 15:15:29 +0900

    Create upload redux & saga & hooks

---
+++ src/data/modules/PostUpload/index.ts
@@ -0,0 +1,143 @@
+const ADD_IMG = 'ADD_IMG';
+const DELETE_IMG = 'DELETE_IMG';
+const CHANGE_DESCRIPTION = 'CHANGE_DESCRIPTION';
+export const UPLOAD_POST = 'UPLOAD_POST';
+export const UPLOAD_POST_SUCCESS = 'UPLOAD_POST_SUCCESS';
+export const UPLOAD_POST_FAILURE = 'UPLOAD_POST_FAILURE';
+
+export interface AddImg {
+  type: typeof ADD_IMG;
+  payload: {
+    newImg: File;
+  };
+}
+
+export interface DeleteImg {
+  type: typeof DELETE_IMG;
+  payload: {
+    index: number;
+  };
+}
+
+export interface ChangeDescription {
+  type: typeof CHANGE_DESCRIPTION;
+  payload: {
+    description: string;
+  };
+}
+
+export interface UploadPost {
+  type:
+    | typeof UPLOAD_POST
+    | typeof UPLOAD_POST_SUCCESS
+    | typeof UPLOAD_POST_FAILURE;
+  payload: {
+    post?: {
+      imgList: File[];
+      description: string;
+      weather: {
+        status: number;
+        temp: number;
+      };
+    };
+    uploadStatus?: 0 | 200 | 400 | 401;
+  };
+}
+
+export const addImg = (img: File): AddImg => ({
+  type: ADD_IMG,
+  payload: {
+    newImg: img,
+  },
+});
+
+export const deleteImg = (index: number): DeleteImg => ({
+  type: DELETE_IMG,
+  payload: {
+    index,
+  },
+});
+
+export const changeDescription = (description: string): ChangeDescription => ({
+  type: CHANGE_DESCRIPTION,
+  payload: {
+    description,
+  },
+});
+
+export const uploadPost = (post: {
+  imgList: File[];
+  description: string;
+  weather: {
+    status: number;
+    temp: number;
+  };
+}): UploadPost => ({
+  payload: {
+    post,
+  },
+  type: UPLOAD_POST,
+});
+
+type PostUploadActionTypes =
+  | AddImg
+  | ChangeDescription
+  | DeleteImg
+  | UploadPost;
+
+interface RootState {
+  imgList: File[];
+  description: string;
+  uploadStatus: 0 | 200 | 400 | 401 | 404;
+}
+
+const initialState: RootState = {
+  description: '',
+  imgList: [],
+  uploadStatus: 0,
+};
+
+const postUploadReducer = (
+  state = initialState,
+  action: PostUploadActionTypes,
+): RootState => {
+  switch (action.type) {
+    case ADD_IMG: {
+      return {
+        ...state,
+        imgList: [...state.imgList, action.payload.newImg],
+      };
+    }
+    case DELETE_IMG:
+      return {
+        ...state,
+        imgList: state.imgList.filter(
+          (img, index) => index !== action.payload.index,
+        ),
+      };
+    case CHANGE_DESCRIPTION:
+      return {
+        ...state,
+        description: action.payload.description,
+      };
+    case UPLOAD_POST:
+      return {
+        ...state,
+        uploadStatus: 0,
+      };
+    case UPLOAD_POST_SUCCESS:
+      return {
+        ...state,
+        uploadStatus: 200,
+      };
+    case UPLOAD_POST_FAILURE:
+      return {
+        ...state,
+        uploadStatus: action.payload.uploadStatus,
+      };
+    default:
+      return state;
+  }
+};
+
+export default postUploadReducer;
