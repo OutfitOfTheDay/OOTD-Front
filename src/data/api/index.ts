93e9f1e1d54a103f5a5ad8ec325dac2ec053b6e1
gimtommang11 <gimtommang88@gmail.com>
2020-06-19 14:41:57 +0900

    change axios to instanceAxios

--- src/data/api/index.ts
+++ src/data/api/index.ts
@@ -2,10 +2,28 @@ import axios from 'axios';
 import * as apiTypes from './apiTypes';
 const baseURL: string = 'http://10.156.145.162:1212';
 
+const instanceAxios = axios.create({
+  baseURL: 'http://10.156.145.162:1212',
+  headers: { userId: '5edc9b14e7179a6b6367fee9' },
+});
+
+export const getWeatherStatusApi = async (payload: {
+  lat: number;
+  lon: number;
+}) => {
+  const response = await instanceAxios.get('/weather', {
+    params: {
+      lat: payload.lat,
+      lon: payload.lon,
+    },
+  });
+  return response.data;
+};
+
 export const getFeedData = async (
   feedParams: apiTypes.FeedRequestParams,
 ): Promise<apiTypes.FeedListType> => {
-  const response = await axios.get(`${baseURL}/feed?`, {
+  const response = await instanceAxios.get(`${baseURL}/feed?`, {
     params: {
       sortN: feedParams.sortN,
       status: feedParams.status,
@@ -14,11 +32,10 @@ export const getFeedData = async (
   });
   return response.data;
 };
-
 export const getCommentData = async (
   commentParams: string,
 ): Promise<apiTypes.CommentDataType> => {
-  const response = await axios.get(`${baseURL}/post/${commentParams}`);
+  const response = await instanceAxios.get(`${baseURL}/post/${commentParams}`);
   return response.data;
 };
 
@@ -26,14 +43,9 @@ export const writingComment = async (
   text: string,
   postId: string,
 ): Promise<number> => {
-  const response = await axios.post(
-    `${baseURL}/comment`,
-    { postId, text },
-    {
-      headers: {
-        userId: '5edc9b14e7179a6b6367fee9',
-      },
-    },
-  );
+  const response = await instanceAxios.post(`${baseURL}/comment`, {
+    postId,
+    text,
+  });
   return response.status;
 };
