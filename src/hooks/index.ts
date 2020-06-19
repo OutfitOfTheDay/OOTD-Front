d93750061f3cc650ea8a0bc2ad6f8de04f38e06a
so_ohi <jsh167528@gmail.com>
2020-06-19 14:56:40 +0900

    Export custom hooks

---
+++ src/hooks/index.ts
@@ -0,0 +1,6 @@
+import usePostUpload from './usePostUpload';
+import useFeedSort from './useFeedSort';
+import useMypage from './useMypage';
+import useWeatherStatus from './useWeatherStatus';
+
+export { useFeedSort, usePostUpload, useMypage, useWeatherStatus };
