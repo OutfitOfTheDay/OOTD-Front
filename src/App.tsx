e60c15b92e248810c262b1173a94ba0276656e15
gimtommang11 <gimtommang88@gmail.com>
2020-06-21 23:28:55 +0900

    Create modal component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { GlobalStyle } from './GlobalStyle';
 import { BrowserRouter } from 'react-router-dom';
 import Router from './Router';
+import Modal from 'modules/Modals';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <GlobalStyle />
+      <Modal />
       <Router />
     </BrowserRouter>
   );
