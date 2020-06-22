178efaa09419c688b2277830393e9d96fb17aba7
gimtommang11 <gimtommang88@gmail.com>
2020-06-22 17:09:51 +0900

    import changeModal hooks in  componet

--- src/components/atoms/Feeds/PostModifyButton/index.tsx
+++ src/components/atoms/Feeds/PostModifyButton/index.tsx
@@ -2,18 +2,27 @@ import React from 'react';
 
 import * as S from './style';
 import useFeed from 'src/hooks/useFeed';
+import useModal from 'src/hooks/useModal';
+import { ModalTypes } from 'src/data/modal/modal';
 
 interface Props {
   fontSize: number;
 }
 const PostModifyButton: React.FC<Props> = ({ fontSize }) => {
   const { isMypage } = useFeed();
+  const { onChangeModal } = useModal();
+
   return (
     <>
       {isMypage ? (
         <S.PostModifyButtonWrapper>
           <S.PostModifyButton fontSize={fontSize}>수정</S.PostModifyButton>
-          <S.PostModifyButton fontSize={fontSize}>삭제</S.PostModifyButton>
+          <S.PostModifyButton
+            fontSize={fontSize}
+            onClick={() => onChangeModal(ModalTypes.DeletePost)}
+          >
+            삭제
+          </S.PostModifyButton>
         </S.PostModifyButtonWrapper>
       ) : (
         <></>
