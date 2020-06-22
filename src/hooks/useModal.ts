f09f531f5c81efa8a2b9376aa0223a74ac1863e4
gimtommang11 <gimtommang88@gmail.com>
2020-06-22 17:10:05 +0900

    delete toggle Modal

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,10 +1,9 @@
 import { useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { StoreState } from '../data/index';
-import { toggleModal, changeModal, ModalTypes } from '../data/modal/modal';
+import { changeModal, ModalTypes } from '../data/modal/modal';
 
 export default function useModal() {
-
   const modalType = useSelector((state: StoreState) => state.modal.modalType);
   const dispatch = useDispatch();
   const onChangeModal = useCallback(
