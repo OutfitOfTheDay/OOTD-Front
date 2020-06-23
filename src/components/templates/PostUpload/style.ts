4354da626f460342b9f0ef28b46e15fe6877068e
so_ohi <jsh167528@gmail.com>
2020-06-23 22:09:15 +0900

    Create post upload template

---
+++ src/components/templates/PostUpload/style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const PostUploadContainer = styled.div`
+  width: 100%;
+  height: calc(100vh - 5rem);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default PostUploadContainer;
