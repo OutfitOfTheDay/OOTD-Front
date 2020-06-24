ab0256656cdb3996f7326108c473c23a9c7b33d8
gimtommang11 <gimtommang88@gmail.com>
2020-06-24 16:05:02 +0900

    create reRenderCount

--- src/hooks/useLikePost.ts
+++ src/hooks/useLikePost.ts
@@ -8,12 +8,16 @@ export default function useLikePost() {
   const likeStatus = useSelector(
     (state: rootState) => state.likePostReducer.LikeStatus,
   );
+  const reRenderCount = useSelector(
+    (state: rootState) => state.likePostReducer.reRenderCount,
+  );
   const dispatch = useDispatch();
   const likePost = useCallback((postId: string) => dispatch(like(postId)), [
     dispatch,
   ]);
 
   return {
+    reRenderCount,
     likeStatus,
     likePost,
   };
