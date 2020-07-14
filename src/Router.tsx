fb7b10406aced2e6dae03e3bc9636215e09cc097
gimtommang11 <gimtommang88@gmail.com>
2020-07-14 22:34:10 +0900

    add EditMode

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,14 +3,22 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 import DetailPost from './components/templates/DetailPost';
 import Feed from './components/templates/Feed';
 import MyPage from './components/templates/MyPageFeed';
-import PostUpload from 'templates/PostUpload';
-
+import PostUpload from './components/templates/PostUpload';
 const Router: React.FC = () => (
   <Switch>
     <Route exact path="/" component={Feed} />
     <Route exact path="/detailPost" component={DetailPost} />
     <Route exact path="/mypage" component={MyPage} />
-    <Route exact path="/upload" component={PostUpload} />
+    <Route
+      exact
+      path="/upload"
+      component={() => <PostUpload isEditMode={false} />}
+    />
+    <Route
+      exact
+      path="/edit"
+      component={() => <PostUpload isEditMode={true} />}
+    />
     <Route component={() => <Redirect to="/" />} />
   </Switch>
 );
